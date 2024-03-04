42a9836b6ac6f756cd552e9c64e57b6d66c4b3c5
giulio-taiocchi <giulio.taiocchi@gmail.com>
2024-03-04 10:57:37 +0000

    debugging Fx Fy inner equation with a source

--- src/AdS4_3_1_s/source.jl
+++ src/AdS4_3_1_s/source.jl
@@ -3,12 +3,12 @@ Base.@kwdef mutable struct NoSource{T} <: Source
 	time :: T = 0.0
 end
 
-Sz(t, x, y, ::NoSource) = 1
-Sz_x(t, x, y, ::NoSource) = 0
-Sz_y(t, x, y, ::NoSource) = 0
-Sz_t(t, x, y, ::NoSource) = 0
-Sz_tx(t, x, y, ::NoSource) = 0
-Sz_ty(t, x, y, ::NoSource) = 0
+Sz(t, x, y, ::NoSource) = 1.0
+Sz_x(t, x, y, ::NoSource) = 0.0
+Sz_y(t, x, y, ::NoSource) = 0.0
+Sz_t(t, x, y, ::NoSource) = 0.0
+Sz_tx(t, x, y, ::NoSource) = 0.0
+Sz_ty(t, x, y, ::NoSource) = 0.0
 
 
 @inline getSourcetime(ff::Source) = ff.time
