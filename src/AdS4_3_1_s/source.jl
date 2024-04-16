ab4ca684fbbcef3040f64c78b836f3a9d2b200cf
giulio-taiocchi <giulio.taiocchi@gmail.com>
2024-04-16 14:14:10 +0100

    added a gaussian source with parametric sigmas

--- src/AdS4_3_1_s/source.jl
+++ src/AdS4_3_1_s/source.jl
@@ -36,28 +36,28 @@ Base.@kwdef mutable struct GaussianSource{T} <: Source
 	sigmay :: T = 0.5
 end
 
-Sz(t, x, y, ::GaussianSource) = 1 + (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*(1 - tanh(12 - t)))/(sigmax*sigmay)
-Sz_x(t, x, y, ::GaussianSource) = -((exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*x*(1 - tanh(12 - t)))/(sigmax^3*sigmay))
-Sz_y(t, x, y, ::GaussianSource) = -((exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*y*(1 - tanh(12 - t)))/(sigmax*sigmay^3))
+Sz(t, x, y, ::GaussianSource) = 1 + (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*(1 - tanh(12 - t)))/(GaussianSource.sigmax*GaussianSource.sigmay)
+Sz_x(t, x, y, ::GaussianSource) = -((exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*x*(1 - tanh(12 - t)))/(GaussianSource.sigmax^3*GaussianSource.sigmay))
+Sz_y(t, x, y, ::GaussianSource) = -((exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*y*(1 - tanh(12 - t)))/(GaussianSource.sigmax*GaussianSource.sigmay^3))
 
-Sz_xx(t, x, y, ::GaussianSource) = ((-(exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))/sigmax^2) + (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*x^2)/sigmax^4)*(1 - tanh(12 - t)))/(sigmax*sigmay)
-Sz_yy(t, x, y, ::GaussianSource) = ((-(exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))/sigmay^2) + (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*y^2)/sigmay^4)*(1 - tanh(12 - t)))/(sigmax*sigmay)
-Sz_xxx(t, x, y, ::GaussianSource) = (((3*exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*x)/sigmax^4 - (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*x^3)/sigmax^6)*(1 - tanh(12 - t)))/(sigmax*sigmay)
-Sz_yyy(t, x, y, ::GaussianSource) = (((3*exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*y)/sigmay^4 - (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*y^3)/sigmay^6)*(1 - tanh(12 - t)))/(sigmax*sigmay)
-Sz_xxxx(t, x, y, ::GaussianSource) = (((3*exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2)))/sigmax^4 - (6*exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*x^2)/sigmax^6 + (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*x^4)/sigmax^8)*(1 - tanh(12 - t)))/(sigmax*sigmay)
-Sz_yyyy(t, x, y, ::GaussianSource) = (((3*exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2)))/sigmay^4 - (6*exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*y^2)/sigmay^6 + (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*y^4)/sigmay^8)*(1 - tanh(12 - t)))/(sigmax*sigmay)
-Sz_xy(t, x, y, ::GaussianSource) = (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*x*y*(1 - tanh(12 - t)))/(sigmax^3*sigmay^3)
-Sz_xxy(t, x, y, ::GaussianSource) = (((exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*y)/(sigmax^2*sigmay^2) - (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*x^2*y)/(sigmax^4*sigmay^2))*(1 - tanh(12 - t)))/(sigmax*sigmay)
-Sz_xyy(t, x, y, ::GaussianSource) = (x*(exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))/sigmay^2 - (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*y^2)/sigmay^4)*(1 - tanh(12 - t)))/(sigmax^3*sigmay)
-Sz_xxyy(t, x, y, ::GaussianSource) = (((exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))/sigmay^2 - (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*y^2)/sigmay^4)/sigmax^2 + (x^2*(-(exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))/sigmay^2) + (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*y^2)/sigmay^4))/sigmax^4)*(1 - tanh(12 - t)))/(sigmax*sigmay)
+Sz_xx(t, x, y, ::GaussianSource) = ((-(exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))/GaussianSource.sigmax^2) + (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*x^2)/GaussianSource.sigmax^4)*(1 - tanh(12 - t)))/(GaussianSource.sigmax*GaussianSource.sigmay)
+Sz_yy(t, x, y, ::GaussianSource) = ((-(exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))/GaussianSource.sigmay^2) + (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*y^2)/GaussianSource.sigmay^4)*(1 - tanh(12 - t)))/(GaussianSource.sigmax*GaussianSource.sigmay)
+Sz_xxx(t, x, y, ::GaussianSource) = (((3*exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*x)/GaussianSource.sigmax^4 - (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*x^3)/GaussianSource.sigmax^6)*(1 - tanh(12 - t)))/(GaussianSource.sigmax*GaussianSource.sigmay)
+Sz_yyy(t, x, y, ::GaussianSource) = (((3*exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*y)/GaussianSource.sigmay^4 - (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*y^3)/GaussianSource.sigmay^6)*(1 - tanh(12 - t)))/(GaussianSource.sigmax*GaussianSource.sigmay)
+Sz_xxxx(t, x, y, ::GaussianSource) = (((3*exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2)))/GaussianSource.sigmax^4 - (6*exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*x^2)/GaussianSource.sigmax^6 + (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*x^4)/GaussianSource.sigmax^8)*(1 - tanh(12 - t)))/(GaussianSource.sigmax*GaussianSource.sigmay)
+Sz_yyyy(t, x, y, ::GaussianSource) = (((3*exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2)))/GaussianSource.sigmay^4 - (6*exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*y^2)/GaussianSource.sigmay^6 + (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*y^4)/GaussianSource.sigmay^8)*(1 - tanh(12 - t)))/(GaussianSource.sigmax*GaussianSource.sigmay)
+Sz_xy(t, x, y, ::GaussianSource) = (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*x*y*(1 - tanh(12 - t)))/(GaussianSource.sigmax^3*GaussianSource.sigmay^3)
+Sz_xxy(t, x, y, ::GaussianSource) = (((exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*y)/(GaussianSource.sigmax^2*GaussianSource.sigmay^2) - (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*x^2*y)/(GaussianSource.sigmax^4*GaussianSource.sigmay^2))*(1 - tanh(12 - t)))/(GaussianSource.sigmax*GaussianSource.sigmay)
+Sz_xyy(t, x, y, ::GaussianSource) = (x*(exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))/GaussianSource.sigmay^2 - (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*y^2)/GaussianSource.sigmay^4)*(1 - tanh(12 - t)))/(GaussianSource.sigmax^3*GaussianSource.sigmay)
+Sz_xxyy(t, x, y, ::GaussianSource) = (((exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))/GaussianSource.sigmay^2 - (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*y^2)/GaussianSource.sigmay^4)/GaussianSource.sigmax^2 + (x^2*(-(exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))/GaussianSource.sigmay^2) + (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*y^2)/GaussianSource.sigmay^4))/GaussianSource.sigmax^4)*(1 - tanh(12 - t)))/(GaussianSource.sigmax*GaussianSource.sigmay)
 
-Sz_t(t, x, y, ::GaussianSource) = (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*sech(12 - t)^2)/(sigmax*sigmay)
-Sz_tx(t, x, y, ::GaussianSource) = -((exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*x*sech(12 - t)^2)/(sigmax^3*sigmay))
-Sz_txx(t, x, y, ::GaussianSource) = ((-(exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))/sigmax^2) + (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*x^2)/sigmax^4)*sech(12 - t)^2)/(sigmax*sigmay)
-Sz_ty(t, x, y, ::GaussianSource) = -((exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*y*sech(12 - t)^2)/(sigmax*sigmay^3))
-Sz_tyy(t, x, y, ::GaussianSource) = ((-(exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))/sigmay^2) + (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*y^2)/sigmay^4)*sech(12 - t)^2)/(sigmax*sigmay)
-Sz_txy(t, x, y, ::GaussianSource) = (exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*x*y*sech(12 - t)^2)/(sigmax^3*sigmay^3)
-Sz_tt(t, x, y, ::GaussianSource) = (2*exp(-1/2*x^2/sigmax^2 - y^2/(2*sigmay^2))*sech(12 - t)^2*tanh(12 - t))/(sigmax*sigmay)
+Sz_t(t, x, y, ::GaussianSource) = (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*sech(12 - t)^2)/(GaussianSource.sigmax*GaussianSource.sigmay)
+Sz_tx(t, x, y, ::GaussianSource) = -((exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*x*sech(12 - t)^2)/(GaussianSource.sigmax^3*GaussianSource.sigmay))
+Sz_txx(t, x, y, ::GaussianSource) = ((-(exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))/GaussianSource.sigmax^2) + (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*x^2)/GaussianSource.sigmax^4)*sech(12 - t)^2)/(GaussianSource.sigmax*GaussianSource.sigmay)
+Sz_ty(t, x, y, ::GaussianSource) = -((exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*y*sech(12 - t)^2)/(GaussianSource.sigmax*GaussianSource.sigmay^3))
+Sz_tyy(t, x, y, ::GaussianSource) = ((-(exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))/GaussianSource.sigmay^2) + (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*y^2)/GaussianSource.sigmay^4)*sech(12 - t)^2)/(GaussianSource.sigmax*GaussianSource.sigmay)
+Sz_txy(t, x, y, ::GaussianSource) = (exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*x*y*sech(12 - t)^2)/(GaussianSource.sigmax^3*GaussianSource.sigmay^3)
+Sz_tt(t, x, y, ::GaussianSource) = (2*exp(-1/2*x^2/GaussianSource.sigmax^2 - y^2/(2*GaussianSource.sigmay^2))*sech(12 - t)^2*tanh(12 - t))/(GaussianSource.sigmax*GaussianSource.sigmay)
 
 
 
