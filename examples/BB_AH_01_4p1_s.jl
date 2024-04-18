f1eacf2aa65c90ab72fd3b5941cecaee93f06e35
giulio-taiocchi <giulio.taiocchi@gmail.com>
2024-04-18 15:05:29 +0100

    parameters added to the function source

--- examples/BB_AH_01_4p1_s.jl
+++ examples/BB_AH_01_4p1_s.jl
@@ -3,13 +3,13 @@ using Jecco.AdS4_3_1_s
 grid = SpecCartGrid3D(
     x_min            = -5.0,
     x_max            =  5.0,
-    x_nodes          =  60,
+    x_nodes          =  96,
     y_min            = -5.0,
     y_max            =  5.0,
-    y_nodes          =  60,
+    y_nodes          =  96,
     u_outer_min      =  0.1,
     u_outer_max      =  1.01,
-    u_outer_domains  =  4,
+    u_outer_domains  =  8,
     u_outer_nodes    =  24,
     u_inner_nodes    =  12,
     fd_order         =  4,
@@ -30,7 +30,7 @@ id = AdS4_3_1_s.BoostedBBnumerical(
 
 
 evoleq = AffineNull(
-    source = GaussianSource(time = 0.0),
+    source = GaussianSource(time = 0.0, sigmax=1,sigmay=1),
     gaugecondition = ConstantAH(u_AH = 1.0),
     #gaugecondition = ConstantAH(u_AH = 0.5),
 )
