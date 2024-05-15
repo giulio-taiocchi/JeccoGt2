98736010141f21c1f7cd1c8ad95983ed24e80734
giulio-taiocchi <giulio.taiocchi@gmail.com>
2024-05-15 20:30:14 +0100

    source function modifications

--- examples/BB_AH_01_4p1_s.jl
+++ examples/BB_AH_01_4p1_s.jl
@@ -24,7 +24,8 @@ id = AdS4_3_1_s.BoostedBBnumerical(
 
 
 evoleq = AffineNull(
-    source = GaussianSource(time = 0.0, sigmax=0.015625,sigmay=0.015625, x0=-2.827433388230814, y0=0.0,Amp=1.0,t0=0.8),
+    source = GaussianSource(time = 0.0, sigmax=1.0,sigmay=1.0, x0=0.0, y0=0.0,Amp=1.0,t0=1.0, L=100000.0),
+    
     gaugecondition = ConstantAH(u_AH = 1.0),
     #gaugecondition = ConstantAH(u_AH = 0.5),
 )
