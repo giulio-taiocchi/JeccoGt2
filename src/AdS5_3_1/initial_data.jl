e05ebef93089609d079943cc409727894aa25e14
giulio-taiocchi <giulio.taiocchi@gmail.com>
2024-06-20 16:48:48 +0100

    adding HDF5 module in 5D version

--- src/AdS5_3_1/initial_data.jl
+++ src/AdS5_3_1/initial_data.jl
@@ -37,6 +37,7 @@ end
 Base.@kwdef struct BoostedBBnumerical{T} <: ID_ConstantAH
     #energy_dens :: T   = 5.0
     AH_pos      :: T   = 1.0
+    phi0          :: T   = 0.0
     ahf         :: AHF = AHF()
 end
 
@@ -913,19 +914,21 @@ end
 
 
 # Numerical boosted Black Brane
-function analytic_B(i, j, k, u, x, y, id::BoostedBBnumerical, whichsystem)
-	uu = u
-	initialB=h5open("/home/giulio/University/PhD/JeccoNewTest/Jecco_G/examples/InitialB_BBB.h5")
-	system_index = string(whichsystem+1)
-	dset=initialB[system_index]
-	B=read(dset)
+analytic_phi(i, j, k, u, x, y, id::BoostedBBnumerical, whichsystem) = 0
+analytic_B1(i, j, k, u, x, y, id::BoostedBBnumerical, whichsystem)  = 0
+analytic_B2(i, j, k, u, x, y, id::BoostedBBnumerical, whichsystem)  = 0
+
+#function analytic_B1(i, j, k, u, x, y, id::BoostedBBnumerical, whichsystem)
+#	uu = u
+#	initialB1=h5open("/home/giulio/University/PhD/JeccoNewTest/Jecco_G/examples/InitialB1_BBB.h5")
+#	system_index = string(whichsystem+1)
+#	dset=initialB1[system_index]
+#	B1=read(dset)
 	# here the indecex have to be inverted since julia and mathematica input and output mechanism is the opposite
 	# should be B[i,j,k]
-	Bvalue = B[k,j,i]
-	
-	
-	Bvalue
-end
+#	Bvalue1 = B1[k,j,i]	
+#	Bvalue1
+#end
 
 function analytic_G(i, j, k, u, x, y, id::BoostedBBnumerical,whichsystem)  
 	uu = u
