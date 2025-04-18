ed516edbd90e1bd44cdddafc7136f4ba6f3c133c
giulio-taiocchi <giulio.taiocchi@gmail.com>
2025-04-18 16:44:35 +0100

    rescaling of u coordinate: trying to figur out why evolution is so slow: rescaled all eqnts in the right way. Got rid of ouput for dbg

--- src/AdS4_3_1_s_rescaled/solve_nested.jl
+++ src/AdS4_3_1_s_rescaled/solve_nested.jl
@@ -873,18 +873,18 @@ function solve_nested!(bulkconstrain::BulkConstrained, bulkevol::BulkEvolved, bc
     # solve for S
     vprint("INFO: solve_S")
     solve_S!(bulk, bc, gauge, deriv, aux_acc, sys, evoleq)
-    println("S solved")
+    #println("S solved")
     # take u-derivatives of S
     vprint("INFO: S derivatives")
     @sync begin
         @spawn mul!(Du_S,   Du,  bulk.S)
         @spawn mul!(Duu_S,  Duu, bulk.S)
     end
-    println("S derivatives written")
+
     # solve for Fx and Fy
     vprint("INFO: solve_Fxy")
     solve_Fxy!(bulk, bc, gauge, deriv, aux_acc, sys, evoleq)
-    println("Fxy solved")
+    #println("Fxy solved")
     # take u-derivatives of Fx and Fy
     vprint("INFO: Fxy derivatives")
     @sync begin
@@ -893,11 +893,11 @@ function solve_nested!(bulkconstrain::BulkConstrained, bulkevol::BulkEvolved, bc
         @spawn mul!(Duu_Fx,  Duu, bulk.Fx)
         @spawn mul!(Duu_Fy,  Duu, bulk.Fy)
     end
-    println("Fxy derivatives written")
+
     # solve for Sd
     vprint("INFO: solve_Sd")
     solve_Sd!(bulk, bc, gauge, deriv, aux_acc, sys, evoleq)
-    println("Sd solved")
+    #println("Sd solved")
     # equations for (Bd, Gd) are actually independent of each
     # other and could be solved in parallel. however, it seems that use of
     # @spawn here is actually harmful for scaling. since the loops in each
@@ -906,11 +906,11 @@ function solve_nested!(bulkconstrain::BulkConstrained, bulkevol::BulkEvolved, bc
 
     vprint("INFO: solve_BdGd")
     solve_BdGd!(bulk, bc, gauge, deriv, aux_acc, sys, evoleq)
-    println("BdGd solved")
+    #println("BdGd solved")
     # solve for A
     vprint("INFO: solve_A")
     solve_A!(bulk, bc, gauge, deriv, aux_acc, sys, evoleq)
-    println("A solved")
+    #println("A solved")
     # take u-derivatives of A. they will be needed for syncing the domains and
     # also for the xi_t function
     vprint("INFO: A derivatives")
