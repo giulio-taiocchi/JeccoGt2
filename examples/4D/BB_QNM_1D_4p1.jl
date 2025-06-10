31375391a4fc6cd407e4ba4046a38a1477a29de7
giulio-taiocchi <giulio.taiocchi@gmail.com>
2025-06-10 12:21:51 +0100

    printing code and soure times

--- examples/4D/BB_QNM_1D_4p1.jl
+++ examples/4D/BB_QNM_1D_4p1.jl
@@ -4,15 +4,15 @@ using Jecco.AdS4_3_1
 grid = SpecCartGrid3D(
     x_min            = -5.0,
     x_max            =  5.0,
-    x_nodes          =  36,
+    x_nodes          =  32,
     y_min            = -5.0,
     y_max            =  5.0,
-    y_nodes          =  64,
-    u_outer_min      =  0.1,
-    u_outer_max      =  1.25,
-    u_outer_domains  =  3,
-    u_outer_nodes    =  24,
-    u_inner_nodes    =  12,
+    y_nodes          =  32,
+    u_outer_min      =  0.55,
+    u_outer_max      =  1.1,
+    u_outer_domains  =  1,
+    u_outer_nodes    =  10,
+    u_inner_nodes    =  10,
     fd_order         =  4,
     sigma_diss       =  0.2,
 )
@@ -28,15 +28,15 @@ evoleq = AffineNull(
 )
 
 io = InOut(
-    out_boundary_every  = 25,
+    out_boundary_every  = 10,
     #out_bulk_every      = 1,
     #out_gauge_every     = 10,
     remove_existing     = true,
 )
 
 integration = Integration(
-    dt              = 0.002,
-    tmax            = 20.0,
+    #dt              = 0.002,
+    tmax            = 200.0,
     ODE_method      = AdS4_3_1.AB3(),
     filter_poststep = true,
 )
